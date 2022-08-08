0442fdc04427e9f6edca47ff4140d284a5388fef
HollaG <31369072+HollaG@users.noreply.github.com>
2022-08-08 11:52:49 +0800

    Create fullscreen_modal.js
    Run this code when 
    - selecting modules
    - selecting classes
    - ranking classes
    to make the modal full screen.

---
+++ fullscreen_modal.js
@@ -0,0 +1,46 @@
+(function adjustCss() {
+    const popup = document.getElementById(
+        `ptModTable_${[0, 1, 2, 3, 4, 5, 6].find((i) =>
+            document.getElementById(`ptModTable_${i}`)
+        )}`
+    );
+    if (!popup) {
+        return console.error("Error - popup element not found");
+    }
+
+    popup.style.left = 0;
+    popup.style.top = 0;
+    popup.style.width = "100%";
+    popup.style.height = "100%";
+
+    const iframe = document.getElementById(
+        `ptModFrame_${[0, 1, 2, 3, 4, 5, 6].find((i) =>
+            document.getElementById(`ptModFrame_${i}`)
+        )}`
+    );
+
+    if (!iframe) {
+        return console.error("Error - iframe element not found");
+    }
+
+    iframe.style.width = "100%";
+    iframe.style.height = "100vh";
+
+    // Observe for changes in the modal element which signifies a user just selected an option
+    const observer = new MutationObserver((mutationList, observer) => {
+        const mutation = mutationList[0];
+
+        if (mutation.type === "attributes") {
+            // Height / Width / whatever changed, run the function again
+            // Check the target element to see if it's at the desired style values. Don't run the function again if the element already has the correct style values
+            if (mutation.target.style.top !== 0) {
+                adjustCss();
+                observer.disconnect();
+            }
+        }
+    });
+
+    observer.observe(popup, {
+        attributes: true,
+    });
+})();
